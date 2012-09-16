4190cce8b5e15219983c59bf4d781efa9e6b7eee
Joshua Holbrook <josh.holbrook@gmail.com>
2012-09-16 12:00:35 -0700

    First commit.

---
+++ proxy.js
@@ -0,0 +1,14 @@
+var myLittleProxy = require('./lib/my-little-proxy');
+
+// Yes, turning on logging is a Whole Thing.
+if (!process.logging) {
+  process.logging = myLittleProxy.logref;
+  myLittleProxy.setupLogger();
+}
+
+myLittleProxy(function (req, res, proxy) {
+  if (this.rewrite('/repository', '/_utils')) {
+    return this.forward('localhost', 5984);
+  }
+  this.end();
+}).listen(8000);

de49c86861ef469b6faaaf87f55036c77c62e290
Joshua Holbrook <josh.holbrook@gmail.com>
2012-09-17 21:49:15 -0700

    [api] Do host and port pattern matching. Also handle case of trying to end connection twice

--- proxy.js
+++ proxy.js
@@ -4,8 +4,19 @@ var myLittleProxy = require('./lib/my-little-proxy');
 myLittleProxy.cli();
 
 myLittleProxy(function (req, res, proxy) {
+  if (this.host('ssh.jesusabdullah.net')) {
+    res.statusCode = 403;
+    res.setHeader('content-type', 'application/json');
+    return res.end(JSON.stringify({
+      error: 'forbidden',
+      message: 'scuse me wtf ur doin',
+      hints: 'go bother someone else.'
+    }, true, 2));
+  }
+
   if (this.rewrite('/couchdb', '/')) {
     return this.forward('localhost', 5984);
   }
+
   this.end();
 }).listen(9090);
